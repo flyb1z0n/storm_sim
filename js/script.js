0cd07f4075c197d2764eb6e40064f2516575c623
flyb1z0n <flyb1z0n@gmail.com>
2021-03-27 22:24:57 +0200

    added resize logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log('script loaded')
+var ctx
+
+function render(){
+    console.log("render")
+    console.log(ctx)
+    ctx.beginPath();
+    ctx.moveTo(10, 10);
+
+    // finish printing border
+    ctx.lineTo(ctx.canvas.width - 10, ctx.canvas.width - 10)
+    ctx.lineTo(10,  ctx.height - 10)
+    // ctx.lineTo(10, 10);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+function init() {
+    canvasEl = document.getElementById('main');
+    ctx = canvasEl.getContext('2d');
+
+    function resizeCanvas() {
+        console.log('resize')
+        canvasEl.style.width = window.innerWidth + "px";
+        canvasEl.width = window.innerWidth;
+        canvasEl.style.height = window.innerHeight + "px";
+        canvasEl.height = window.innerHeight;
+    };
+    window.onresize = resizeCanvas;
+    resizeCanvas();
+
+    setInterval(render, 1000);
+    
+}
+
+init()
+
+
