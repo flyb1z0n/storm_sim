414b56f2e1433ada2bc4aedf92fd3e2b4df108d8
flyb1z0n <flyb1z0n@gmail.com>
2021-04-12 23:34:06 +0300

    added lighting generation

--- js/script.js
+++ js/script.js
@@ -2,20 +2,18 @@ var ctx
 var borderSize = 0;
 var hasZip = false;
 
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
+document.body.onkeyup = function (e) {
+    if (e.keyCode == 32) {
         clean();
     }
 }
 
-function clean()
-{
+function clean() {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     printBorder();
 }
 
-function printBorder()
-{
+function printBorder() {
     ctx.beginPath();
     ctx.moveTo(borderSize, borderSize);
     ctx.lineTo(ctx.canvas.width - borderSize, borderSize)
@@ -26,11 +24,10 @@ function printBorder()
     ctx.stroke();
 }
 
-function clickHandler() 
-{
-    hasZip = true;   
-   
-  
+function clickHandler() {
+    hasZip = true;
+
+    clean();
     render();
 
 }
@@ -41,102 +38,147 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function printZip() 
-{   
-    var maxIteration = 1;
-    var maxOffset = 100;
+function generateZip(initSegment)
+{
+    var maxIteration = 7;
+    var chanceToBranch = 30;
+    var segments = [];
 
-    ///
+ 
+    segments.push(
+        initSegment
+    )
 
-    var pos_x = ctx.canvas.width /2;
-    var pos_y = ctx.canvas.height /2;
-
-    var points = [];
-
-    points.push({ x : pos_x, y : pos_y,})
-    points.push({ x : pos_x, y : 0,})
-
-    printLine(points)
-
-    function normal(x1,y1,x2,y2,x3,y3)
-    {
-        k = ((y2-y1) * (x3-x1) - (x2-x1) * (y3-y1)) / ((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
-        console.log(k)
-        x4 = x3 - k * (y2-y1);
-        y4 = y3 + k * (x2-x1);
-        return {x : x4, y: y4}
-    }
-
-    for (var i = 0; i < maxIteration; i++)
-    {
+    for (var i = 0; i < maxIteration; i++) {
         var result = [];
-        for(var idx = 0; idx < points.length - 1; idx++ )
-        {
-            var mid_pos_x = (points[idx].x  + points[idx+1].x) / 2;
-            var mid_pos_y = (points[idx].y  + points[idx+1].y) / 2;
-            // should find a normal vector and point that has random ofseest
-            var normal = normal(points[idx].x, points[idx].y,  points[idx+1].x,  points[idx+1].y, mid_pos_x , mid_pos_y)
-            console.log(normal)
-            console.log({x : mid_pos_x, y : mid_pos_y})
-            result.push(points[idx])
-            result.push({x : mid_pos_x, y : mid_pos_y})
-            result.push(points[idx +1 ])
-            console.log({x : mid_pos_x, y:  mid_pos_y})
+        for (var idx = 0; idx < segments.length; idx++) {
+            var currSeg = segments[idx];
+            var length = Math.sqrt((currSeg.start.x - currSeg.end.x) * (currSeg.start.x - currSeg.end.x) + (currSeg.start.y - currSeg.end.y) * (currSeg.start.y - currSeg.end.y))
+            var maxOffset = length / 4;
+            var mid_point = {
+                x: (currSeg.start.x + currSeg.end.x) / 2,
+                y: (currSeg.start.y +  currSeg.end.y) / 2
+            };
+
+           
+
+            /*
+            Чтобы получить вектор 2D, перпендикулярный другому вектору 2D, вы можете просто поменять местами компоненты X и Y и отрицать новую компоненту Y. Таким образом, { x, y } становится { y | - x }.
+            */
+            var normal = {
+                x: (currSeg.end.y - mid_point.y),
+                y: -1 * (currSeg.end.x - mid_point.x)
+            };
+            // normalization
+            var normal_len = Math.sqrt(normal.x * normal.x + normal.y * normal.y)
+            normal = {
+                x: normal.x / normal_len,
+                y: normal.y / normal_len
+            };
+
+            var offset = getRandomInt(-1 * maxOffset, maxOffset)
+            mid_point = {
+                x: mid_point.x + offset * normal.x,
+                y: mid_point.y + offset * normal.y,
+            }
+
+            result.push({
+                start: currSeg.start,
+                end: mid_point,
+                isBranch : currSeg.isBranch
+            })
+
+            result.push({
+                start: mid_point,
+                end: currSeg.end,
+                isBranch : currSeg.isBranch
+            })
+            
+            if(i != maxIteration -1  && Math.random() < (chanceToBranch/100))
+            {
+                var direction = {
+                    x :  mid_point.x - currSeg.start.x,
+                    y : mid_point.y  -  currSeg.start.y
+                }
+
+                var directionLength = Math.sqrt(direction.x * direction.x + direction.y * direction.y)
+                direction = {
+                    x: direction.x / directionLength,
+                    y: direction.y / directionLength
+                };
+
+                var branch = Object.assign({}, mid_point)
+
+
+                branch = {
+                    x: branch.x + length * direction.x / 2,
+                    y: branch.y + length * direction.y / 2,
+                }
+
+                result.push({
+                    start: mid_point,
+                    end: branch,
+                    isBranch: true
+                })
+            }
+            
         }
-        points = result;
-        printLine(points)
+        segments = result;
     }
-
-
-
-    
-
-
-    // print stroke:
-   
-
-
-
-
-
-    // bad generation for zip ot the right corner
-    // ctx.beginPath();
-    // ctx.moveTo(pos_x, pos_y);
-    // // ctx.lineTo(0, 200)
-    // // ctx.lineTo(200, 0)
-    // while (pos_x > borderSize && pos_x < ctx.canvas.width - borderSize && 
-    //       pos_y > borderSize && pos_y < ctx.canvas.height)
-    // {
-    //     var dest_post_x = getRandomInt(pos_x - 5, pos_x + 10);
-    //     var dest_post_y = getRandomInt(pos_y - 5, pos_y + 10);
-    //     console.log({x : dest_post_x, y : dest_post_y})
-    //     ctx.lineTo(dest_post_x, dest_post_y)
-    //     pos_x = dest_post_x;
-    //     pos_y = dest_post_y;
-    // }
-    // ctx.stroke();
-    console.log('Zip')
+    return segments;
 }
 
-function printLine(points)
+function printZip() {
+    var pos_x = ctx.canvas.width / 2;
+    var pos_y = ctx.canvas.height / 2;
+    var initialSegment =   {
+        start: { x: pos_x, y: 0 },
+        end: { x: pos_x, y: ctx.canvas.height }
+    }
+    var segments1 = generateZip(
+        initialSegment
+    );
+
+    var segments2 = generateZip(
+        initialSegment
+    );
+   
+    printSegments(segments1)
+    printSegments(segments2)
+}
+
+function printSegments(segments)
 {
+   
+    segments.forEach(seg => {
+        ctx.beginPath();
+        ctx.strokeStyle = 'white';
+        ctx.moveTo(seg.start.x, seg.start.y);
+        ctx.lineTo(seg.end.x, seg.end.y);
+        ctx.lineWidth = seg.isBranch ? 1 : 2;
+        ctx.stroke()
+    });
+   
+}
+
+function printLine(points) {
     ctx.beginPath();
-    ctx.moveTo(points[0].x,points[0].y)
+    ctx.moveTo(points[0].x, points[0].y)
     points.forEach(el => {
         ctx.lineTo(el.x, el.y);
     });
+    ctx.lineWidth = 2;
     ctx.stroke()
 }
 
 
-function render(){
+function render() {
     console.log("render")
     printBorder()
 
-    if(hasZip)
-    {
-        printZip() 
-    }    
+    if (hasZip) {
+        printZip()
+    }
     hasZip = false;
 
 }
@@ -158,9 +200,9 @@ function init() {
     window.onresize = resizeCanvas;
     resizeCanvas();
 
-  
-    // setInterval(render, 1000);
-    
+
+    setInterval(clickHandler, 200);
+
 }
 
 init()
